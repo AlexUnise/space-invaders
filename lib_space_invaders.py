23d985e67f66465bf297550d9abe4bead479b4e4
Alexandre <alexandre.ouenadio@cpe.fr>
2021-12-14 12:14:56 +0100

    Description classe Player

--- lib_space_invaders.py
+++ lib_space_invaders.py
@@ -8,6 +8,12 @@ Description du fichier: Bibilothèque contenant toutes les fonctions et classes 
 
 
 class Player:
+    #Classe Joueur
+    #Un joueur peut se déplacer uniquement sur l'axe x. Le programmeur définit sa coordonnée y. 
+    #dx réprésente la vitesse de déplacement sur l'axe, définie par le programmeur.
+    #La fonction draw déssine pour la première fois le player sur le canvas
+    #Les fonctions moveRight et moveLeft permettent de déplacer le joueur sur le canvas.
+    
     def __init__(self,x,y,dx,width,height,canvas):
         self.x = x
         self.y = canvas.cget('height') - y
