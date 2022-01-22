25621ad75134a258a928e3c4fe8d6ff1dbffec98
Zaph <zmichalop@gmail.com>
2022-01-22 17:41:25 +0100

    mouvement bloc

--- lib_space_invaders.py
+++ lib_space_invaders.py
@@ -32,10 +32,10 @@ class Space_Invaders:
         
 
         #Aliens
-        self.__alien=Alien(self.__canvas,self.__wind)
-        self.__queue=[]
-        self.__aliens=2
-        
+        # self.__alien=Alien(self.__canvas,self.__wind,0,0)
+        # self.__queue=[]
+        # self.__aliens=2
+        self.__blocAlien=Bloc(self.__canvas,self.__wind)
         
         #Joueuer
         self.__player=Player(int(self.__width/2),self.__height-50,20,50,50,self.__canvas,self.__wind)
@@ -63,9 +63,9 @@ class Space_Invaders:
         newGameButton.grid(column=1, row=0, sticky="s")
         quitButton.grid(column=1, row=1, sticky='n')
         
-        self.__alien.place_aliens()
+        self.__blocAlien.create_bloc()
         self.__player.place_player()
-        self.__alien.move_alien()
+        self.__blocAlien.move_bloc()
         self.__canvas.focus_set()
         self.__canvas.bind('<Key>',self.keyboard)
         
@@ -87,9 +87,9 @@ class Space_Invaders:
         if key=='q' and x0-self.__player.dx>0:
             self.__player.moveLeft(self.__canvas)   
         if key=='f':
-            projectile_joueur=Projectile(self.__canvas,self.__wind,self.__player,self.__backImg)
-            projectile_joueur.placer_projectile()
-            projectile_joueur.tirer_projectile()
+            projectilePlayer=Projectile(self.__canvas,self.__wind,self.__player,self.__backImg)
+            projectilePlayer.place_projectile()
+            projectilePlayer.fire_projectile()
             
         
 #classe Alien pour attribuer le nombre de vie, la cadence de tir et le comportment de l'alien       
@@ -98,39 +98,38 @@ class Alien:
     #Chaque Alien à une coordonnée x et une coordonnée y.
     #On definit sa taille ainsi que sa hitbox
     #dx et dy réprésentent sa vitesse sur chaque axe
-    def __init__(self,canvas,wind):
+    def __init__(self,canvas,wind,positionx,positiony):
         self.__lives=1
         self.__hitbox=50
         self.width = 50
         self.height = 50
-        self.__positionx=0
-        self.__positiony=0
+        self.__positionx=positionx
+        self.__positiony=positiony
         self.__dx = 10
-        self.__dy = 0
-        self.__queue=[]
+        self.__dy = 30
         self.__canvas=canvas
         self.__wind=wind
+        self.__alien=self.__canvas.create_rectangle (self.__positionx,self.__positiony,self.__positionx+self.width,self.__positiony+self.height,fill="red")
     
 
-    def move_alien(self):
-        (x0,y0,x1,y1)=self.__canvas.coords(self.__queue[0])
-        if x1+(self.__dx)>int(self.__canvas.cget('width')):
-            self.__dx=-self.__dx
-        if x0+self.__dx<0:
-            self.__dx=-self.__dx
-            self.__dy=50
-        self.__canvas.move(self.__queue[0],self.__dx,self.__dy )
-        self.__dy=0
-        self.__wind.after(20,self.move_alien)
+    def move_alien(self,signe,DownMovement):
+        
+        (x0,y0,x1,y1)=self.__canvas.coords(self.__alien)
+        self.__canvas.move(self.__alien,signe*self.__dx,DownMovement*self.__dy )
+            
 
-    def place_aliens(self):
-        self.__queue.append(self.__canvas.create_rectangle (self.__positionx,self.__positiony,self.__positionx+self.width,self.__positiony+self.height,fill="red"))
-
+    def border_overlapping(self,signe,DownMovement):
+        (x0,y0,x1,y1)=self.__canvas.coords(self.__alien)
+        if x1+(signe*self.__dx)>int(self.__canvas.cget('width')):
+            signe=-1
+            DownMovement=1
+        elif x0+(signe*self.__dx)<0:
+            signe=1
+            DownMovement=1
+        return signe,DownMovement
     
-
-    
-    
-    
+    def alien_hit(self):
+        self.__alien
 
     
     
@@ -175,33 +174,64 @@ class Projectile:
         self.__entity=entity
         self.__backImg=backImg
         
-    def placer_projectile(self):
+    def place_projectile(self):
         (x0,y0,x1,y1)=self.__canvas.coords(self.__entity.rect)
         self.projectile=self.__canvas.create_rectangle(x0+int(self.__entity.width/2),y0-5,x0+int(self.__entity.width/2)+self.__width,y0-5-self.__height,fill="yellow")
         
     
-    def tirer_projectile(self):
+    def fire_projectile(self):
         
         (x_proj0,y_proj0,x_proj1,y_proj1)=self.__canvas.coords(self.projectile)
         liste_objet=self.__canvas.find_overlapping(x_proj0,y_proj0,x_proj1,y_proj1)
         print(liste_objet)
         if y_proj0-self.__dy>0:
             self.__canvas.move(self.projectile,0,-self.__dy)
-            self.__wind.after(20,self.tirer_projectile)
+            self.__wind.after(20,self.fire_projectile)
         elif len(liste_objet)!=0:
-            self.__canvas.delete(self.projectile)
+           
             for objet in liste_objet:
                 if objet!=self.__backImg:
+
                     self.__canvas.delete(objet)
         else:
             self.__canvas.delete(self.projectile)
-        
+            
 
 
 
 class Bloc:
-    def __init__(self):
+    def __init__(self,canvas,wind):
+        self.__canvas=canvas
+        self.__wind=wind
         self.__height=3
-        self.__width
-    
-    
+        self.__width=8
+        self.__aliens=[]
+        self.__positionx=0
+        self.__positiony=0
+        self.__signeDx=1
+        self.__DownMovement=0
+    def create_bloc(self):
+        placementy=self.__positiony
+        for columns in range(0,self.__height):
+            ligne_alien=[]
+            placementx=self.__positionx
+            
+            for rows in range(0,self.__width):
+                alien=Alien(self.__canvas,self.__wind,placementx,placementy)
+                ligne_alien.append(alien)
+                placementx+=60
+            self.__aliens.append(ligne_alien)
+            placementy+=60
+    def move_bloc(self):
+        self.__DownMovement=0
+        for ligne in self.__aliens:
+            if self.__signeDx==1:
+                [self.__signeDx,self.__DownMovement]=ligne[-1].border_overlapping(self.__signeDx,self.__DownMovement)
+                
+            elif self.__signeDx==-1:
+                [self.__signeDx,self.__DownMovement]=ligne[0].border_overlapping(self.__signeDx,self.__DownMovement)
+
+        for ligne in self.__aliens:
+            for alien in ligne:
+                alien.move_alien(self.__signeDx,self.__DownMovement)
+        self.__wind.after(120,self.move_bloc)
